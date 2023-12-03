7d10a8668e55f0b4233db9595a384e1425bc6968
Walid Saifi <saifiwalid21@gmail.com>
2023-12-03 20:58:20 +0100

    update

--- Walid/data.sql
+++ Walid/data.sql
@@ -4,25 +4,32 @@ CREATE TABLE Articles (
     Contenu TEXT NOT NULL,
     DatePublication TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     HeurePublication TIME DEFAULT CURRENT_TIME,
-    AuteurID INT,
-    FOREIGN KEY (AuteurID) REFERENCES Utilisateurs(UserID)
+    id_user INT,
+    FOREIGN KEY (id_user) REFERENCES Utilisateurs(id_user)
 );
 
 
 CREATE TABLE Utilisateurs (
-    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT PRIMARY KEY AUTO_INCREMENT,
     Nom VARCHAR(50) NOT NULL,
     Email VARCHAR(100) UNIQUE NOT NULL,
-    MotDePasse VARCHAR(255) NOT NULL
+    MotDePasse VARCHAR(255) NOT NULL,
+    id_role INT 
+    FOREIGN KEY (id_role) REFERENCES role(id_role)
+);
+CREATE TABLE roles (
+    id_role INT AUTO_INCREMENT PRIMARY KEY,
+    role VARCHAR(50) 
+    
 );
 CREATE TABLE Permissions (
-    PermissionID INT PRIMARY KEY AUTO_INCREMENT,
+    id_Permission INT PRIMARY KEY AUTO_INCREMENT,
     NomPermission VARCHAR(50) NOT NULL
 );
 CREATE TABLE UtilisateursPermissions (
-    UserID INT,
-    PermissionID INT,
-    PRIMARY KEY (UserID, PermissionID),
-    FOREIGN KEY (UserID) REFERENCES Utilisateurs(UserID) ON DELETE CASCADE,
-    FOREIGN KEY (PermissionID) REFERENCES Permissions(PermissionID) ON DELETE CASCADE
+    id_user INT,
+    id_Permission INT,
+    PRIMARY KEY (id_user, id_Permission),
+    FOREIGN KEY (id_user) REFERENCES Utilisateurs(id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_Permission) REFERENCES Permissions(id_Permission) ON DELETE CASCADE
 );
