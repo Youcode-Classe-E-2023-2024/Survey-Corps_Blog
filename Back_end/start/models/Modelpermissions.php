009b49b4d2bb2f5a2809d02016a19bd81e8935b9
Walid Saifi <saifiwalid21@gmail.com>
2023-12-08 19:58:46 +0100

    Update Modelpermissions.php

--- Back_end/start/models/Modelpermissions.php
+++ Back_end/start/models/Modelpermissions.php
@@ -1,6 +1,6 @@
 <?php
 include "C:/xampp/htdocs/Survey-Corps_Blog/Back_end/backwork/Dataopp.php";
 
-$Admin = new DatabaseConnect('phpframe', 'localhost', 'root', '');
+$Admin = new DatabaseConnect('phpframe1', 'localhost', 'root', '');
 
 
