8733cae2aa7ef30de475650e3089842a895c75bb
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-08 20:06:17 +0100

    Update ControllerPUsers.php

--- Back_end/start/controllers/POST/ControllerPUsers.php
+++ Back_end/start/controllers/POST/ControllerPUsers.php
@@ -6,7 +6,6 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){;
         // add filter later
         var_dump($data);
         echo $this->senderid; // hna ghanzid check dyal permision 
-        $Users->Insert("Utilisateurs",$data);
         
         
 
