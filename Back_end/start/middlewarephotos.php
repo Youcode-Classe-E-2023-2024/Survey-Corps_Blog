b6646e1b37711d587cf0bbef0814c91a3eed66bd
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-09 16:40:38 +0100

    Update middlewarephotos.php

--- Back_end/start/middlewarephotos.php
+++ Back_end/start/middlewarephotos.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $targetDir = 'uploads/';
+    $targetDir = 'storage/';
     $targetFile = $targetDir . basename($_FILES['file']['name']);
     
     if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)) {
